2426b128a8a8de91d8fb874db21bb39f9a5defc8
laky-r-k <lakshmishrkanchan12@gmail.com>
2026-09-21 00:21:27 +0530

    clean up

--- server.py
+++ server.py
@@ -4,14 +4,12 @@ import uvicorn
 import time
 
 from agents.state import TraceFixState
-from controller.event_router import EventRouter, IncidentStore
 from controller.pworkflow import Eventhandler
 from core.phoenix_setup import setup_phoenix
 
 from graphs.diagnosis_graph import create_diagnosis_graph
 from graphs.judge_graph import create_judge_graph
 
-from services.github_service import GitHubService
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 from config import GEMINI_API_KEY
@@ -32,8 +30,7 @@ llm = ChatGoogleGenerativeAI(
 DIAGNOSIS_GRAPH = create_diagnosis_graph(llm)
 JUDGE_GRAPH = create_judge_graph(llm)
 
-github_service = GitHubService()
-incident_store = IncidentStore()
+
 
 router = Eventhandler(
     DIAGNOSIS_GRAPH,
@@ -44,7 +41,7 @@ router = Eventhandler(
 @app.post("/github-webhook")
 async def github_webhook(request: Request):
     payload = await request.json()
-    print(payload)
+    #print(payload)
 
     event = {
         "repo": payload["repository"]["full_name"],
